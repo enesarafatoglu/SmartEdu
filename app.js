b5ea5b53ddcfdf2b52fa17492fcba724a6e86e8c
Enes Arafatoglu <enessarafatogluu@gmail.com>
2024-06-21 22:04:30 +0300

    Register page created

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const pageRoute = require('./routes/pageRoute');
 const courseRoute = require('./routes/courseRoute');
 const categoryRoute = require('./routes/categoryRoute');
+const userRoute = require('./routes/userRoute');
 
 const app = express();
 
@@ -27,8 +28,8 @@ app.use(express.urlencoded({ extended: true }));
 //Routes
 app.use('/', pageRoute);
 app.use('/courses', courseRoute);
-app.use('/courses', courseRoute);
 app.use('/categories', categoryRoute);
+app.use('/users', userRoute)
 
 
 const port = 3000;
