d93d53cd205d1ae59ae2b966e34c9637151856d9
Enes Arafatoglu <enessarafatogluu@gmail.com>
2024-06-21 22:37:56 +0300

    Login page created

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 exports.createUser = async (req, res) => {
   try {
@@ -7,6 +8,37 @@ exports.createUser = async (req, res) => {
       status: 'success',
       user,
     });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
+    });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+      if (!user) {
+        return res.status(401).json({
+          status: 'fail',
+          message: 'Incorrect email or password',
+        });
+      }
+      const same = await bcrypt.compare(password, user.password);
+
+      if(!same) {
+        return res.status(401).json({
+          status: 'fail',
+          message: 'Incorrect email or password',
+        });
+      }
+
+      //USER SESSION
+      res.status(200).send('YOU ARE LOGGED IN');
+
   } catch (error) {
     res.status(400).json({
       status: 'fail',
