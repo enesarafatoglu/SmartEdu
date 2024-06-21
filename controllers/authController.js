03cfff9e84def373f6e53a5e8d729afbea00738e
Enes Arafatoglu <enessarafatogluu@gmail.com>
2024-06-21 23:55:54 +0300

    Special page created

--- controllers/authController.js
+++ controllers/authController.js
@@ -38,7 +38,7 @@ exports.loginUser = async (req, res) => {
 
     //USER SESSION
     req.session.userID = user._id;
-    res.status(200).redirect('/');
+    res.status(200).redirect('/users/dashboard');
   } catch (error) {
     res.status(400).json({
       status: 'fail',
@@ -50,5 +50,13 @@ exports.loginUser = async (req, res) => {
 exports.logoutUser = (req, res) => {
   req.session.destroy(() => {
     res.redirect('/');
+  });
+};
+
+exports.getDashboardPage = async (req, res) => {
+  const user = await User.findOne({_id: req.session.userID});
+  res.status(200).render('dashboard', {
+    page_name: 'dashboard',
+    user
   });
 };
