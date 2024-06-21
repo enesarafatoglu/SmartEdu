1308207e437b348da6d76ff6d01b8d3da9fd55dc
Enes Arafatoglu <enessarafatogluu@gmail.com>
2024-06-21 23:32:05 +0300

    Exit process done

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoStore = require('connect-mongo');
 const pageRoute = require('./routes/pageRoute');
 const courseRoute = require('./routes/courseRoute');
 const categoryRoute = require('./routes/categoryRoute');
@@ -24,16 +25,18 @@ app.set('view engine', 'ejs');
 //Global Variable
 global.userIN = null;
 
-
 //MIDDLEWARES
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(session({
-  secret: 'my_keyboard_cat',
-  resave: false,
-  saveUninitialized: true,
-}))
+app.use(
+  session({
+    secret: 'my_keyboard_cat',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: 'mongodb://localhost/smartedu-db' }),
+  })
+);
 
 //Routes
 app.use('*', (req, res, next) => {
@@ -43,8 +46,7 @@ app.use('*', (req, res, next) => {
 app.use('/', pageRoute);
 app.use('/courses', courseRoute);
 app.use('/categories', categoryRoute);
-app.use('/users', userRoute)
-
+app.use('/users', userRoute);
 
 const port = 3000;
 
--- controllers/authController.js
+++ controllers/authController.js
@@ -21,29 +21,34 @@ exports.loginUser = async (req, res) => {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
 
-      if (!user) {
-        return res.status(401).json({
-          status: 'fail',
-          message: 'Incorrect email or password',
-        });
-      }
-      const same = await bcrypt.compare(password, user.password);
+    if (!user) {
+      return res.status(401).json({
+        status: 'fail',
+        message: 'Incorrect email or password',
+      });
+    }
+    const same = await bcrypt.compare(password, user.password);
 
-      if(!same) {
-        return res.status(401).json({
-          status: 'fail',
-          message: 'Incorrect email or password',
-        });
-      }
+    if (!same) {
+      return res.status(401).json({
+        status: 'fail',
+        message: 'Incorrect email or password',
+      });
+    }
 
-      //USER SESSION
-      req.session.userID = user._id;
-      res.status(200).redirect('/');
-
+    //USER SESSION
+    req.session.userID = user._id;
+    res.status(200).redirect('/');
   } catch (error) {
     res.status(400).json({
       status: 'fail',
       error,
     });
   }
+};
+
+exports.logoutUser = (req, res) => {
+  req.session.destroy(() => {
+    res.redirect('/');
+  });
 };
