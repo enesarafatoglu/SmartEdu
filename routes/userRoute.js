b8a6f8cf756aa8216dc2b96d3dbf46073adb50aa
Enes Arafatoglu <enessarafatogluu@gmail.com>
2024-06-22 00:28:01 +0300

    Special middlewares created

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const authController = require('../controllers/authController');
+const authMiddleWare = require('../middlewares/authMiddleWare');
 
 const router = express.Router();
 
 router.route('/signup').post(authController.createUser);
 router.route('/login').post(authController.loginUser);
 router.route('/logout').get(authController.logoutUser);
-router.route('/dashboard').get(authController.getDashboardPage);
+router.route('/dashboard').get(authMiddleWare, authController.getDashboardPage);
 
 
 module.exports = router;
