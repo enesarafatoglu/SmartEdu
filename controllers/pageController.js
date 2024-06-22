4b5c74849bf67f1c47197bc962204e109f78d7f2
Enes Arafatoglu <enessarafatogluu@gmail.com>
2024-06-23 02:07:11 +0300

    Contact page added

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,3 +1,5 @@
+const nodemailer = require('nodemailer');
+
 exports.getIndexPage = (req, res) => {
   console.log(req.session.userID);
   res.status(200).render('index', {
@@ -21,4 +23,46 @@ exports.getLoginPage = (req, res) => {
   res.status(200).render('login', {
     page_name: 'login',
   });
+};
+
+exports.getContactPage = (req, res) => {
+  res.status(200).render('contact', {
+    page_name: 'contact',
+  });
+};
+
+exports.sendEmail = async (req, res) => {
+  
+  const outputMessage = `
+  
+  <h1>Mail Details</h1>
+  <ul>
+    <li>Name: ${req.body.name}</li> 
+    <li>Email: ${req.body.email}</li> 
+  </ul>
+  <h1>Message</h1>
+  <p>${req.body.message}</p>
+  `
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // Use `true` for port 465, `false` for all other ports
+    auth: {
+      user: "", 
+      pass: "", 
+    },
+  });
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: '"Smart EDU Contact Form 👻" <>', // sender address
+      to: "", // list of receivers
+      subject: "Hello ✔", // Subject line
+      html: outputMessage, // html body
+    });
+  
+    //console.log("Message sent: %s", info.messageId);
+    //console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+
+    res.status(200).redirect('contact');
 };
--- routes/pageRoute.js
+++ routes/pageRoute.js
@@ -8,6 +8,8 @@ router.route('/').get(pageController.getIndexPage);
 router.route('/about').get(pageController.getAboutPage);
 router.route('/register').get(redirectMiddleWare, pageController.getRegisterPage);
 router.route('/login').get(redirectMiddleWare, pageController.getLoginPage);
+router.route('/contact').get(pageController.getContactPage);
+router.route('/contact').post(pageController.sendEmail);
 
 module.exports = router;
 
