ad03e4f80abd61eea83d4f509fbd9b087c9ac2eb
Enes Arafatoglu <enessarafatogluu@gmail.com>
2024-06-22 14:48:02 +0300

    User roles created

--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const courseController = require('../controllers/courseController');
+const roleMiddleWare = require('../middlewares/roleMiddleWare');
 
 const router = express.Router();
 
-router.route('/').post(courseController.createCourse);
+router.route('/').post(roleMiddleWare(["teacher", "admin"]), courseController.createCourse);
 router.route('/').get(courseController.getAllCourses);
 router.route('/:slug').get(courseController.getCourse);
 
