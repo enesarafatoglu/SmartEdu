cab96b402c7671e440becd56f4eb12fa9b611800
Enes Arafatoglu <enessarafatogluu@gmail.com>
2024-06-23 00:54:08 +0300

    Course - Student relationship || made

--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -8,6 +8,7 @@ router.route('/').post(roleMiddleWare(["teacher", "admin"]), courseController.cr
 router.route('/').get(courseController.getAllCourses);
 router.route('/:slug').get(courseController.getCourse);
 router.route('/enroll').post(courseController.enrollCourse);
+router.route('/release').post(courseController.releaseCourse);
 
 module.exports = router;
 
