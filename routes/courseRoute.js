484e98f85d1e4e7dddc2fdbcfc06735a4ff11c0c
Enes Arafatoglu <enessarafatogluu@gmail.com>
2024-06-23 18:32:06 +0300

    Course updated

--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -8,6 +8,7 @@ router.route('/').post(roleMiddleWare(["teacher", "admin"]), courseController.cr
 router.route('/').get(courseController.getAllCourses);
 router.route('/:slug').get(courseController.getCourse);
 router.route('/:slug').delete(courseController.deleteCourse);
+router.route('/:slug').put(courseController.updateCourse);
 router.route('/enroll').post(courseController.enrollCourse);
 router.route('/release').post(courseController.releaseCourse);
 
